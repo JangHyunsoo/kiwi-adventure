37de430b3ea6131d8a63d0878da469a302b88ddf
JangHyunsoo <hunsoo911@gmail.com>
2022-01-24 14:08:36 +0900

    fix: edit InGameScene

--- Assets/Scripts/SkillEquipSlot.cs
+++ Assets/Scripts/SkillEquipSlot.cs
@@ -5,24 +5,25 @@ using UnityEngine.UI;
 
 public class SkillEquipSlot : MonoBehaviour
 {
+    [SerializeField]
     private Image image_;
     private Skill skill_;
+    private Sprite none_sprite_;
 
     void Start()
     {
-        image_ = GetComponent<Image>();
+        none_sprite_ = image_.sprite;
     }
 
     public void updateSlot(Skill _skill)
     {
         skill_ = _skill;
         image_.sprite = skill_.skill_icon;
-        setColor(1);
     }
     public void clearSlot()
     {
         skill_ = null;
-        // setColor(0);
+        image_.sprite = null;
     }
     private void setColor(float _alpha)
     {
