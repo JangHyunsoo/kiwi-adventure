cc8d21ad77b43685ef48fa91f02619c26c0badb2
JangHyunsoo <hunsoo911@gmail.com>
2022-03-30 13:49:19 +0900

    fix: back Player.prefab fix: del other SkillDataBase

--- Assets/Scripts/TestSkill/SkillData.cs
+++ Assets/Scripts/TestSkill/SkillData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
+[CreateAssetMenu(fileName = "SkillData.asset", menuName = "SkillData")]
 public class SkillData : ScriptableObject
 {
     public int skill_no;
