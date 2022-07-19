ebfc9bfe27c95e11dd9cc70a0f3892a05f78f8db
JangHyunsoo <hunsoo911@gmail.com>
2022-07-19 14:14:41 +0900

    edit : skill slider ui script next prev function edit (slot circle logic edit)

--- Assets/Scripts/SkillSliderUI.cs
+++ Assets/Scripts/SkillSliderUI.cs
@@ -16,9 +16,20 @@ public class SkillSliderUI : MonoBehaviour
     private float speed_ = 5f;
 
     private const int SKILL_SIZE_ = 5;
+    private const int SLOT_SIZE_ = 8;
 
+    [SerializeField]
     private int front_slot_index_ = 0;
+    [SerializeField]
     private int end_slot_index_ = 4;
+
+    [SerializeField]
+    private int cur_pos_index_ = 1;
+    [SerializeField]
+    private int cur_skill_index_ = 1;
+
+    [SerializeField]
+    private int[] real_idx_pos_ = { 0, 1, 2, 3, 4 };
 
     [SerializeField]
     private Transform skill_slot_image_parent_;
@@ -29,14 +40,11 @@ public class SkillSliderUI : MonoBehaviour
     [SerializeField]
     private Transform[] skill_slot_pos_list_;
 
-    private int cur_index_ = 1;
-
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Z))
         {
-            Debug.Log(cur_index_.ToString());
             rotateNext();
         }
         else if (Input.GetKeyDown(KeyCode.X))
@@ -58,9 +66,9 @@ public class SkillSliderUI : MonoBehaviour
 
     private void updateSkillSlotPos()
     {
-        for (int i = 0; i < skill_slot_pos_list_.Length; i++)
+        for (int i = 0; i < SKILL_SIZE_; i++)
         {
-            skill_slot_image_list_[i].position = skill_slot_pos_list_[i].position;
+            skill_slot_image_list_[i].position = skill_slot_pos_list_[real_idx_pos_[i]].position;
         }
     }
 
@@ -79,7 +87,7 @@ public class SkillSliderUI : MonoBehaviour
         for (int i = 0; i < SKILL_SIZE_; i++)
         {
             var cur_image = skill_slot_image_list_[i];
-            if (cur_index_ == i)
+            if (cur_skill_index_ == i)
             {
                 cur_image.localScale = Vector3.Lerp(Vector3.one, cur_image.localScale, Time.deltaTime * speed_);
             }
@@ -92,15 +100,43 @@ public class SkillSliderUI : MonoBehaviour
 
     private void rotateNext()
     {
-        if(cur_index_ < 1)
-        {
+        var new_front_idx = (front_slot_index_ - 1) % SKILL_SIZE_;
+        if (new_front_idx < 0) new_front_idx += SKILL_SIZE_;
 
+        if (cur_pos_index_ - real_idx_pos_[front_slot_index_] == 1) 
+        {
+            var new_front_real_pos = (real_idx_pos_[front_slot_index_] - 1) % SLOT_SIZE_;
+            if (new_front_real_pos < 0) new_front_real_pos += SLOT_SIZE_;
+            real_idx_pos_[new_front_idx] = new_front_real_pos;
+            front_slot_index_ = new_front_idx;
+            end_slot_index_ = (end_slot_index_ - 1) % SKILL_SIZE_;
+            if (end_slot_index_ < 0)
+            {
+                end_slot_index_ += SKILL_SIZE_;
+            }
+        }
+        else if (cur_pos_index_ == 0 && real_idx_pos_[front_slot_index_] == (SLOT_SIZE_ - 1))
+        {
+            real_idx_pos_[new_front_idx] = 6;
+            front_slot_index_ = new_front_idx;
+            end_slot_index_ = (end_slot_index_ - 1) % SKILL_SIZE_;
+            if (end_slot_index_ < 0)
+            {
+                end_slot_index_ += SKILL_SIZE_;
+            }
         }
 
-        cur_index_ = (cur_index_ - 1) % SKILL_SIZE_;
-        if (cur_index_ < 0)
+        cur_pos_index_ = (cur_pos_index_ - 1) % SLOT_SIZE_;
+        cur_skill_index_ = (cur_skill_index_ - 1) % SKILL_SIZE_;
+
+        if (cur_pos_index_ < 0)
         {
-            cur_index_ += SKILL_SIZE_;
+            cur_pos_index_ += SLOT_SIZE_;
+        }
+
+        if (cur_skill_index_ < 0)
+        {
+            cur_skill_index_ += SKILL_SIZE_;
         }
 
         target_rotation_ = Quaternion.Euler(new Vector3(0, 0, target_rotation_.eulerAngles.z - 45));
@@ -108,17 +144,27 @@ public class SkillSliderUI : MonoBehaviour
 
     private void rotatePrivious()
     {
-        cur_index_ = (cur_index_ + 1) % SKILL_SIZE_;
-        if (cur_index_ < 0)
+        var new_end_idx = (end_slot_index_ + 1) % SKILL_SIZE_;
+
+        if (cur_pos_index_ - real_idx_pos_[end_slot_index_] == -1)
         {
-            cur_index_ += SKILL_SIZE_;
+            var new_end_real_pos = (real_idx_pos_[end_slot_index_] + 1) % SLOT_SIZE_;
+            real_idx_pos_[new_end_idx] = new_end_real_pos;
+            end_slot_index_ = new_end_idx;
+            front_slot_index_ = (front_slot_index_ + 1) % SKILL_SIZE_;
         }
+        else if (cur_pos_index_ == (SLOT_SIZE_ - 1) && real_idx_pos_[end_slot_index_] == 0)
+        {
+            real_idx_pos_[new_end_idx] = 1;
+            end_slot_index_ = new_end_idx;
+            front_slot_index_ = (front_slot_index_ + 1) % SKILL_SIZE_;
+        }
+
+        cur_pos_index_ = (cur_pos_index_ + 1) % SLOT_SIZE_;
+        cur_skill_index_ = (cur_skill_index_ + 1) % SKILL_SIZE_;
 
         target_rotation_ = Quaternion.Euler(new Vector3(0, 0, target_rotation_.eulerAngles.z + 45));
     }
-
-
-
 
 
  //   private void moveCurrSkillCursorWithKeyboard()
